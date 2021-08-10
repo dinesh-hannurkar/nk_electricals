c4fdbccc287419c3dc7d41d8d5eccff6f38d0be1
dineshhannurkar <dineshhannurkar20@gmail.com>
2021-08-10 20:52:56 +0530

    dashboard, header, login panel added

--- nk_electricals/Form1.Designer.cs
+++ nk_electricals/Form1.Designer.cs
@@ -28,57 +28,126 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.textBox1 = new System.Windows.Forms.TextBox();
-            this.textBox2 = new System.Windows.Forms.TextBox();
-            this.button1 = new System.Windows.Forms.Button();
+            this.main_panel = new System.Windows.Forms.Panel();
+            this.header = new System.Windows.Forms.Panel();
+            this.label1 = new System.Windows.Forms.Label();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.login_panel = new System.Windows.Forms.Panel();
+            this.txt_username = new System.Windows.Forms.TextBox();
+            this.txt_password = new System.Windows.Forms.TextBox();
+            this.btn_login = new System.Windows.Forms.Button();
+            this.main_panel.SuspendLayout();
+            this.header.SuspendLayout();
+            this.panel1.SuspendLayout();
+            this.login_panel.SuspendLayout();
             this.SuspendLayout();
             // 
-            // textBox1
+            // main_panel
             // 
-            this.textBox1.Location = new System.Drawing.Point(610, 71);
-            this.textBox1.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
-            this.textBox1.Name = "textBox1";
-            this.textBox1.Size = new System.Drawing.Size(148, 26);
-            this.textBox1.TabIndex = 0;
+            this.main_panel.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.main_panel.Controls.Add(this.panel1);
+            this.main_panel.Controls.Add(this.header);
+            this.main_panel.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.main_panel.Location = new System.Drawing.Point(0, 0);
+            this.main_panel.Name = "main_panel";
+            this.main_panel.Size = new System.Drawing.Size(1096, 624);
+            this.main_panel.TabIndex = 0;
             // 
-            // textBox2
+            // header
             // 
-            this.textBox2.Location = new System.Drawing.Point(610, 126);
-            this.textBox2.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
-            this.textBox2.Name = "textBox2";
-            this.textBox2.Size = new System.Drawing.Size(148, 26);
-            this.textBox2.TabIndex = 1;
+            this.header.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(64)))), ((int)(((byte)(80)))), ((int)(((byte)(141)))));
+            this.header.Controls.Add(this.label1);
+            this.header.Dock = System.Windows.Forms.DockStyle.Top;
+            this.header.Location = new System.Drawing.Point(0, 0);
+            this.header.Name = "header";
+            this.header.Size = new System.Drawing.Size(1094, 43);
+            this.header.TabIndex = 0;
             // 
-            // button1
+            // label1
             // 
-            this.button1.Location = new System.Drawing.Point(574, 188);
-            this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(75, 23);
-            this.button1.TabIndex = 2;
-            this.button1.Text = "button1";
-            this.button1.UseVisualStyleBackColor = true;
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Yu Gothic UI", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.ForeColor = System.Drawing.Color.White;
+            this.label1.Location = new System.Drawing.Point(11, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(157, 25);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "NK ELECTRICALS";
+            // 
+            // panel1
+            // 
+            this.panel1.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.panel1.Controls.Add(this.login_panel);
+            this.panel1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.panel1.Location = new System.Drawing.Point(0, 43);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(1094, 579);
+            this.panel1.TabIndex = 1;
+            // 
+            // login_panel
+            // 
+            this.login_panel.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.login_panel.Controls.Add(this.btn_login);
+            this.login_panel.Controls.Add(this.txt_password);
+            this.login_panel.Controls.Add(this.txt_username);
+            this.login_panel.Location = new System.Drawing.Point(306, 153);
+            this.login_panel.Name = "login_panel";
+            this.login_panel.Size = new System.Drawing.Size(481, 271);
+            this.login_panel.TabIndex = 0;
+            // 
+            // txt_username
+            // 
+            this.txt_username.Location = new System.Drawing.Point(176, 69);
+            this.txt_username.Name = "txt_username";
+            this.txt_username.Size = new System.Drawing.Size(100, 20);
+            this.txt_username.TabIndex = 0;
+            // 
+            // txt_password
+            // 
+            this.txt_password.Location = new System.Drawing.Point(176, 116);
+            this.txt_password.Name = "txt_password";
+            this.txt_password.Size = new System.Drawing.Size(100, 20);
+            this.txt_password.TabIndex = 1;
+            // 
+            // btn_login
+            // 
+            this.btn_login.Location = new System.Drawing.Point(176, 154);
+            this.btn_login.Name = "btn_login";
+            this.btn_login.Size = new System.Drawing.Size(75, 23);
+            this.btn_login.TabIndex = 2;
+            this.btn_login.Text = "button1";
+            this.btn_login.UseVisualStyleBackColor = true;
             // 
             // Form1
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(1200, 692);
-            this.Controls.Add(this.button1);
-            this.Controls.Add(this.textBox2);
-            this.Controls.Add(this.textBox1);
-            this.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.ClientSize = new System.Drawing.Size(1096, 624);
+            this.Controls.Add(this.main_panel);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
             this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "Form1";
+            this.main_panel.ResumeLayout(false);
+            this.header.ResumeLayout(false);
+            this.header.PerformLayout();
+            this.panel1.ResumeLayout(false);
+            this.login_panel.ResumeLayout(false);
+            this.login_panel.PerformLayout();
             this.ResumeLayout(false);
-            this.PerformLayout();
 
         }
 
         #endregion
 
-        private System.Windows.Forms.TextBox textBox1;
-        private System.Windows.Forms.TextBox textBox2;
-        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Panel main_panel;
+        private System.Windows.Forms.Panel header;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Panel login_panel;
+        private System.Windows.Forms.Button btn_login;
+        private System.Windows.Forms.TextBox txt_password;
+        private System.Windows.Forms.TextBox txt_username;
     }
 }
 
